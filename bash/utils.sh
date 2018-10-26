4df90282148c81fe2feb966599db532f4fa1ead7
NIck Murray <njmurray@umich.edu>
2018-10-26 12:55:09 -0400

    Update utils.sh

--- bash/utils.sh
+++ bash/utils.sh
@@ -12,16 +12,20 @@ is_interactive() {
     [[ "$-" == *i* ]]
 }
 
+# Debug
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
 # Assert
 # Evaluates all arguments as $@
 # Silent on success, loud on fail
 assert() {
-    ASSERT_MSG="Assertion failed:"
     FAIL_CMD=$(is_interactive && echo "return" || echo "exit")
     [ -z "$1" ] && echo "$ASSERT_MSG No command given." && $FAIL_CMD 1
     $@ 2>/dev/null 1>&2
     EXIT_STAT=$?
     if [ $EXIT_STAT -ne 0 ]; then
-        echo "$ASSERT_MSG $@" && $FAIL_CMD $EXIT_STAT
+        yell "assertion failed: $*`1" && $FAIL_CMD $EXIT_STAT
     fi
 }
