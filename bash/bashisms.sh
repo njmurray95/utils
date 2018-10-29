d275ae77c5528da7ab47720b80be468ba212cfd8
NIck Murray <njmurray@umich.edu>
2018-10-29 13:31:18 -0400

    Update bashisms.sh

--- bash/bashisms.sh
+++ bash/bashisms.sh
@@ -6,3 +6,18 @@
 # Reads from $@ if set, stdin otherwise
 # Evaluates via $(())
 calc() { echo "$((${@:-$(</dev/stdin)}))"; }
+
+# Print all 256 colors in foreground
+# The magic escape code here is "\e[38;5;${color}m"
+# \e[0m clears the color
+colors_fg()
+{
+    for i in {1..256}; do echo -e "\e[38;5;${i}mcolor${i} \e[0m"; done
+}
+
+# Prints all 256 colors in background
+# Here the escape code is "\e[48;5;${color}m"
+colors_bg()
+{
+    for i in {1..256}; do echo -e "\e[48;5;${i}m color${i} \e[0m; done
+}
