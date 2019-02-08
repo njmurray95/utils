18fe56986e7ee327b1dc7e14152c8c4dbc7d9760
NIck Murray <njmurray@umich.edu>
2019-02-08 10:08:29 -0500

    Update utils.sh

--- bash/utils.sh
+++ bash/utils.sh
@@ -12,16 +12,20 @@ is_interactive() {
     [[ "$-" == *i* ]]
 }
 
-# True when executable in path false otherwise
-exists() {
-    which "$1" >/dev/null 2>&1
-}
-
 # Debug: Three-Fingered Claw. See here:
 # https://stackoverflow.com/questions/1378274/#25515370
 yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
+
+# True when executable in path false otherwise
+exists() {
+    which "$1" >/dev/null 2>&1
+}
+
+require() {
+    exists "$1" || die "No $1 in path."
+}
 
 # Assert
 # Evaluates all arguments as $@
