e9168a540322a5f1b68d2606794e8457fbf84aee
njmurray <njmurray@umich.edu>
2019-02-08 23:33:31 -0500

    moving things over from bashrc

--- bash/utils.sh
+++ bash/utils.sh
@@ -3,12 +3,12 @@
 # Utilities  for Bash
 
 # True when the user is root
-is_root() {
+isRoot() {
     [ "$EUID" -eq 0 ]
 }
 
 # True in terminal false in script
-is_interactive() {
+isInteractive() {
     [[ "$-" == *i* ]]
 }
 
@@ -25,17 +25,4 @@ exists() {
 
 require() {
     exists "$1" || die "No $1 in path."
-}
-
-# Assert
-# Evaluates all arguments as $@
-# Silent on success, loud on fail
-assert() {
-    FAIL_CMD=$(is_interactive && echo "return" || echo "exit")
-    [ -z "$1" ] && echo "$ASSERT_MSG No command given." && $FAIL_CMD 1
-    $@ 2>/dev/null 1>&2
-    EXIT_STAT=$?
-    if [ $EXIT_STAT -ne 0 ]; then
-        yell "assertion failed: $*" && $FAIL_CMD $EXIT_STAT
-    fi
 }
