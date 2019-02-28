ed46534b4f2ce5dbca9401d3fd38269e80d2cc7f
NIck Murray <njmurray@umich.edu>
2019-02-28 14:09:15 -0500

    Adding MessageBox example

--- c++/winUtils.cpp
+++ c++/winUtils.cpp
@@ -2,14 +2,20 @@
 #error
 #endif
 
+#include <string>
 #include <Windows.h>
 
-bool processRunning(const LPCSTR procName)
+bool processRunning(const std::string& pName)
 {
-    HWND hwnd = FindWindow(NULL, pName);
+    HWND hwnd = FindWindow(NULL, pName.c_str());
     if (hwnd != 0)
     { 
         return true;
     }
     return false;
+}
+
+void display(const std::string& message, const std::string& title="")
+{
+    MessageBox(NULL, title.c_str(), message.c_str(), MB_OK); 
 }
