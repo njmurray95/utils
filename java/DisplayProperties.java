d40305b2b7c86e1bf7573deda2bf167429c98b2f
NIck Murray <njmurray@umich.edu>
2019-03-21 08:46:41 -0400

    Update DisplayProperties.java

--- java/DisplayProperties.java
+++ java/DisplayProperties.java
@@ -2,11 +2,8 @@
 import java.util.Properties;
 
 public class DisplayProperties {
-
     public static void main(String[] args) {
-
         Properties properties = System.getProperties();
-        // Java 8
         properties.forEach((k, v) -> System.out.println(k + ":" + v));
     }
 }
