d9994c81dd7180358c9af82a0419e008eb39696d
NIck Murray <njmurray@umich.edu>
2020-03-11 11:31:38 -0400

    added prompt to show pwd in window title

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -3,3 +3,12 @@
 # Test-Path $Profile
 
 Set-Location C:\Users\nmurray\code
+
+Function prompt {
+    # Change the window-title to show PWD
+    $host.ui.RawUI.WindowTitle = $(get-location)
+
+    # Default PS Prompt
+    # from: (get-childitem -path Function:\prompt).scriptblock
+    "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
+}
