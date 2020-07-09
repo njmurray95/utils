84ca21be0cb82fcbd723cfe24205b8e75beb986c
NIck Murray <njmurray@umich.edu>
2020-07-09 11:46:02 -0400

    Update winUtils.cpp

--- c++/winUtils.cpp
+++ c++/winUtils.cpp
@@ -5,6 +5,26 @@
 #include <string>
 #include <Windows.h>
 
+// Launch subprocess
+// Equivalent to fork + exec
+void fork(const std::string& command)
+{
+    
+    // Process overhead
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    // Launch process
+    // See: https://docs.microsoft.com/en-us/windows/win32/procthread/creating-processes
+    CreateProcess(NULL, const_cast<char*>(command.c_str()),
+                  NULL, NULL, FALSE, 0, NULL, NULL,
+                  &si, &pi);
+}
+
 bool processRunning(const std::string& pName)
 {
     HWND hwnd = FindWindow(NULL, pName.c_str());
