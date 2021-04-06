72064203559a2218e14867ed438bf588d5b33a8e
NIck Murray <njmurray@umich.edu>
2021-04-06 09:46:21 -0400

    Create getprogs.sh

---
+++ systems/provisioning/getprogs.sh
@@ -0,0 +1,22 @@
+# Modified from: https://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+fi
+
+if [[ "$OS" == "Ubuntu" ]]; then
+    PACMAN="apt-get install";
+fi
+
+PACKAGES="ack"
