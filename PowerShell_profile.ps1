ee6581a6295e4dea1bfd5396ab61971568971f26
NIck Murray <njmurray@umich.edu>
2021-04-13 13:26:53 -0400

    Update PowerShell_profile.ps1

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # PowerShell Profile
 #
-# Test-Path $Profile
+# Powershell looks for this file wherever $Profile is set
+# i.e., $profile, nvim $Profile, Test-Path $Profile, etc.
 
 Set-Location C:\Users\nmurray\code
 
