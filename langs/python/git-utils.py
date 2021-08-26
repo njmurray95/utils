0968f7faa530c74d218ec199045f18f0719812e3
Nick Murray <njmurray@umich.edu>
2021-08-26 20:55:36 +0000

    git-utils.py created online with Bitbucket

---
+++ langs/python/git-utils.py
@@ -0,0 +1,10 @@
+# pip install gitpython
+import git
+
+def swap_to_master():
+    repo = git.Repo(search_parent_directories=True)
+    
+    current = repo.head.ref
+    master = repo.heads.master
+    
+    r.head.reference = master
