15f72af0d17c61b229b814d34942e7d4674ba60a
Nick Murray <njmurray@umich.edu>
2023-08-31 14:58:31 -0400

    Draft method to do tee in python

--- langs/python/snafutils/printutils.py
+++ langs/python/snafutils/printutils.py
@@ -3,7 +3,23 @@
 
 from __future__ import print_function # Python 2 compatibility
 
+import sys
+
 # See also: import pprint
+
+class Tee:
+    '''
+    Write output to an artibrary number of file descriptors.
+    '''
+    def __init__(self, fd_list):
+        self.fd_list = fd_list
+    def __del__(self):
+        pass
+    def write(self, text):
+        for fd in self.fd_list:
+            fd.write(text)
+    def flush(self):
+        pass
 
 def dict_print(dictionary):
     import json
@@ -35,3 +51,10 @@ def side_by_side_diff(a, b):
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+    with open('test.txt', 'w') as testlog:
+        with open('testtwo.txt', 'w') as testtwolog:
+            sys.stdout = Tee([sys.stdout, testlog, testtwolog])
+            print('test')
+
+
