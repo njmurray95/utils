4b7fc2e3a9873d1964616a8c6624fa78fcbfcf73
Nick Murray <njmurray@umich.edu>
2023-10-17 13:13:59 -0400

    Wallpaper script

---
+++ dotfiles/wallpaper.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# wallpaper.sh
+
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+QUIET=0
+WALLPAPER=""
+
+while [[ "$1" ]]; do
+    case $1 in
+        -h | --help)
+            echo "$0: [-q|--quiet] [-p|--path] </path/to/wallpaper/dir> [-w|--wallpaper] </path/to/wallpaper>"
+            exit 0
+            ;;
+        -p | --path)
+            if [[ "$2" ]]; then
+                WALLPAPER_DIR="$2"
+                shift
+            else
+                echo "$0: Error: --path requires a directory."
+            fi
+            ;;
+        -w | --wallpaper)
+            if [[ "$2" ]]; then
+                WALLPAPER="$2"
+                shift
+            else
+                echo "$0: Error: --wallpaper requires a file."
+            fi
+            ;;
+        -q | --quiet)
+            QUIET=1
+            ;;
+        *)
+            echo "$0: Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [[ -z "$WALLPAPER" ]]; then
+    if [[ ! -d "$WALLPAPER_DIR" ]]; then
+        echo "Wallpaper dir $WALLPAPER_DIR does not exit"
+        exit 1
+    fi
+
+    # Select a random wallpaper
+    WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+fi
+
+if [[ ! -e "$WALLPAPER" ]]; then
+    echo "Error: could not find wallpaper $WALLPAPER"
+    exit 1
+fi
+
+
+if [[ "$QUIET" -ne 1 ]]; then
+    echo "Setting wallpaper to $WALLPAPER"
+fi
+
+# Set it
+if [[ $XDG_CURRENT_DESKTOP == *"GNOME" ]]; then
+    # Have to use gsettings
+    gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER"
+    gsettings set org.gnome.desktop.background picture-options "scaled"
+    # For other options, see:
+    # `gsettings range org.gnome.desktop.background picture-options
+else
+    feh --bg-fill "$WALLPAPER"
+fi
+
