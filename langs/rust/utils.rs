56ce17b812dc30250dba395d3c58b2e2f05da04d
Nick Murray <njmurray@umich.edu>
2024-05-06 11:47:46 -0400

    Update utils.rs

--- langs/rust/utils.rs
+++ langs/rust/utils.rs
@@ -1,3 +1,9 @@
-fn print_type_of<T>(_: &T) {
+use std::path::Path;
+
+pub fn print_type_of<T>(_: &T) -> () {
     println!("{}", std::any::type_name::<T>())
+}
+
+pub fn running_in_docker() -> bool {
+    Path::new("/.dockerenv").exists()
 }
